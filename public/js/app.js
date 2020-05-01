fb1a214cf5c3320079db253d8199d6a58727e39d
venkatesh <venkatesh.marreboyina@soulpageit.com>
2020-05-01 15:58:00 +0530

    adding turn and stun servers

--- public/js/app.js
+++ public/js/app.js
@@ -169,10 +169,17 @@ connection.processSdp = function (sdp) {
 connection.iceServers = [
   {
     urls: [
-      "stun:stun.l.google.com:19302",
-      "stun:stun1.l.google.com:19302",
-      "stun:stun2.l.google.com:19302",
-      "stun:stun.l.google.com:19302?transport=udp",
+      // "stun:stun.l.google.com:19302",
+      // "stun:stun1.l.google.com:19302",
+      // "stun:stun2.l.google.com:19302",
+      // "stun:stun.l.google.com:19302?transport=udp",
+      "stun:bn-turn1.xirsys.com",
+      "turn:bn-turn1.xirsys.com:80?transport=udp",
+      "turn:bn-turn1.xirsys.com:3478?transport=udp",
+      "turn:bn-turn1.xirsys.com:80?transport=tcp",
+      "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+      "turns:bn-turn1.xirsys.com:443?transport=tcp",
+      "turns:bn-turn1.xirsys.com:5349?transport=tcp",
     ],
   },
 ];
